948481846a4c54c85bec4ba3faaf81a65583e051
Jakub Coufal <coufalja@gmail.com>
2025-05-05 07:43:29 +0200

    Add prettier and eslint.config.js

---
+++ frontend/eslint.config.js
@@ -0,0 +1,60 @@
+import { defineConfig } from "eslint/config";
+import eslint from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import importPlugin from 'eslint-plugin-import';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default defineConfig([
+    eslint.configs.recommended,
+    {
+        files: ['**/*.{ts,tsx,js,jsx}'],
+        plugins: {
+            '@typescript-eslint': tseslint,
+            'react': reactPlugin,
+            'react-hooks': reactHooksPlugin,
+            'import': importPlugin,
+            'jsx-a11y': jsxA11yPlugin,
+            'prettier': prettierPlugin,
+        },
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+            'import/resolver': {
+                typescript: {},
+            },
+        },
+        rules: {
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            'import/order': [
+                'error',
+                {
+                    'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+                    'newlines-between': 'always',
+                    'alphabetize': { order: 'asc', caseInsensitive: true }
+                }
+            ],
+            'prettier/prettier': 'error',
+        },
+    },
+    prettierConfig,
+]);
